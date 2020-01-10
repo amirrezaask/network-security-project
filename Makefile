274b14bed56675018347bf39d85cfc02be98cf46
amirrezaask <raskarpour@gmail.com>
2020-01-10 20:09:45 +0330

    almost done

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-run:
-	gunicorn api:app
+sample-generate-keys:
+	python3 main.py --mode g
+sample-encrypt:
+	python3 main.py --mode e --e 7 --d 91635976162903 --n 160362984049267 --inpt SALAM
+sample-decrypt:
+	python3 main.py --mode d --e 7 --d 91635976162903 --n 160362984049267 --cipher 124576655406223
---
+++ main.py
@@ -0,0 +1,40 @@
+import click
+
+from rsa import rsa
+
+'''
+Sample Usage:
+    Generate:
+        python3 main.py --mode g
+    Encrypt:
+        python3 main.py --mode e --e 7 --d 91635976162903 --n 160362984049267 --inpt SALAM
+
+    Decrypt:
+        python3 main.py --mode d --e 7 --d 91635976162903 --n 160362984049267 --cipher 124576655406223
+
+
+'''
+
+@click.command()
+@click.option('--mode', '-m')
+@click.option('--n')
+@click.option('--e')
+@click.option('--d')
+@click.option('--inpt')
+@click.option('--cipher')
+def main(mode, n, e, d, inpt, cipher):
+    if mode == 'g' or mode == 'generate' or mode == 'generate-keys':
+        n, e, d = rsa.KeyGenerator.create_rsa_keys()
+        print('n = {}, e={}, d={}'.format(n, e, d))
+
+    elif mode == 'e' or mode == 'encrypt':
+        print('n = {}, e={}, d={}'.format(n, e, d))
+        r = rsa.RSA(int(n), int(e), int(d))
+        print(r.encrypt(inpt))
+    elif mode == 'd' or mode == 'decrypt':
+        print('n = {}, e={}, d={}'.format(n, e, d))
+        r = rsa.RSA(int(n), int(e), int(d))
+        print(r.decrypt(int(cipher)))
+
+if __name__ == '__main__':
+    main()
